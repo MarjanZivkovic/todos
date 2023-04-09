aad4c3c043252ae0a0d7412483230f2bf793ef76
MarjanZivkovic <zivkovicmarjan@yahoo.com>
2023-04-09 16:21:23 +0200

    editing todos enabled

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import { v4 as uuidv4 } from "uuid";
 import { useState, useEffect } from "react";
 import Button from "./components/Button";
 import Form from "./components/Form";
-import check from "./check.svg"
+import check from "./check.svg";
+import { FaTimes, FaEdit } from "react-icons/fa";
 
 function App() {
+  // fetch todos from LStorage or empty arr
   const [ todos, setTodos ] = useState(() =>{
     const savedTodos = localStorage.getItem('todos');
     const initialTodos = JSON.parse(savedTodos);
     return initialTodos || [];
   });
+  // states for open form and edit todos
   const [ openInput, setOpenInput ] = useState(false);
+  const [ todoEdit, setTodoEdit ] = useState({
+    todo: {},
+    isEdit: false
+  });
 
+  // saving to LS on every state change
   useEffect(() =>{
     localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos])
+  }, [todos]);
 
+  // open and close form functions
   function openFormHandler(){
     setOpenInput(true);
   }
-
   function closeForm(){
     setOpenInput(false);
   }
-
+  // create ne todo function
   function createToDoHandler( createdToDo ){
     const newToDo ={
       id: uuidv4(),
@@ -33,11 +41,21 @@ function App() {
     setTodos([newToDo, ...todos]);
     setOpenInput(false);
   }
-
+  // delete to do
   function deleteToDo(id){
     setTodos(todos.filter((todo) => todo.id !== id ));
   }
 
+  // edit todo
+  function editToDo( todo ){
+    setTodoEdit({
+      todo,
+      isEdit: true
+    });
+    setOpenInput(true);
+  }
+
+  // toggle completed class
   function toggleCompleteTodo(id){
     setTodos(todos.map( todo =>{
         if( todo.id === id ){
@@ -47,9 +65,20 @@ function App() {
         }
         return todo;
       })
-    )
+    );
   }
 
+  // update todos with edited todo
+  function updateTodos(id, updatedTodo){
+    setTodos( todos.map( todo => todo.id === id ? { ...todo, name: updatedTodo }  : todo ) );
+
+    setOpenInput(false);
+
+    setTodoEdit({
+      todo: {},
+      isEdit: false
+    });
+}
 
   return (
     <div className="App">
@@ -62,16 +91,17 @@ function App() {
               <li className="todo-li" key={todo.id}>
                 <span className="check-img">{todo.completed ? <img src={check} alt="check mark"/> : null}</span>
                 <span className={todo.completed ? 'completed' : ''} onClick={() => toggleCompleteTodo(todo.id)}>{todo.name}</span> 
-                <button onClick={() => deleteToDo(todo.id)}>x</button>
+                <button onClick={() => editToDo(todo)}><FaEdit /></button>
+                <button onClick={() => deleteToDo(todo.id)}><FaTimes /></button>
               </li>)
             })}
           </ul>
         }
-        {openInput && <Form  onCreateToDO={createToDoHandler}  closeForm={closeForm}/>}
+        {openInput && <Form  onCreateToDO={createToDoHandler}  closeForm={closeForm} todoEdit={todoEdit} setTodoEdit={setTodoEdit} updateTodos={updateTodos}/>}
         <Button  onOpenForm={openFormHandler} />                         
       </main>
       <footer>
-        <p><strong>+</strong> to add a todo, <strong>click</strong> on it to toggle completed, <strong>x</strong> to delete it</p>
+        <p><strong>+</strong> to add a todo, <strong>click</strong> on it to toggle completed, <FaEdit style={{transform: 'translateY(0.15rem)'}}/> to edit it, <FaTimes style={{transform: 'translateY(0.2rem)'}}/> to delete it</p>
         <p className="created-by">created by <a href="https://marjan-zivkovic.com/" target="_blank" rel="noreferrer">marjan</a></p>
       </footer>
     </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function Form({ onCreateToDO, closeForm }){
+function Form({ onCreateToDO, closeForm, todoEdit, setTodoEdit, updateTodos }){
     const [ enteredTodo, setEnteredTodo ] = useState('');
     const [ btnDisabled, setBtnDisabled ] = useState(true);
 
+    // side effect to get edited todo
+    useEffect(() =>{
+        if(todoEdit.isEdit === true){
+            setEnteredTodo(todoEdit.todo.name);
+            setBtnDisabled(false);
+        }
+    }, [todoEdit]);
+
+    // update state with user's input
     function handleChange(e){
         if(e.target.value !== ''){
             setBtnDisabled(false);
@@ -11,27 +20,39 @@ function Form({ onCreateToDO, closeForm }){
         setEnteredTodo(e.target.value);
     }
 
+    // on form submition either create or edit todo
     function submitHandler(e){
         e.preventDefault();
+
         if(enteredTodo.trim().length > 0){
-            onCreateToDO(enteredTodo);
+            if( todoEdit.isEdit === true ){
+                updateTodos( todoEdit.todo.id, enteredTodo  )
+            } else {
+                onCreateToDO(enteredTodo);
+            }
+
+            setEnteredTodo('');
+            setBtnDisabled(true);
         }
-        setEnteredTodo('');
     }
 
     function closeHandler(e){
         if(e.target.classList.contains('form-container')){
            closeForm();
+           setTodoEdit({
+            todo: {},
+            isEdit: false
+          });    
         }
     }
 
     return (
         <div className="form-container" onClick={closeHandler}>
             <form onSubmit={submitHandler}>
-                <h2>Add new todo!</h2>
+                {todoEdit.isEdit ? <h2>Edit todo!</h2> : <h2>Add new todo!</h2> }
                 <div className="form-group">
                     <input type="text" onChange={handleChange} value={enteredTodo} autoFocus/>
-                    <button  type="submit" disabled={btnDisabled}>Add</button>
+                    <button  type="submit" disabled={btnDisabled}>{ todoEdit.isEdit ? 'Save' : 'Add' }</button>
                 </div>
             </form>
         </div>
